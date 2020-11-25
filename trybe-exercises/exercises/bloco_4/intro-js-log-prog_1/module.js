fe77d8194e78b00de0dddc5bd51bdee03943dcc3
Pablo Fernandes <pabloprates@gmail.com>
2020-11-25 18:08:28 -0300

    The module program

---
+++ trybe-exercises/exercises/bloco_4/intro-js-log-prog_1/module.js
@@ -0,0 +1,12 @@
+/**
+ * This is a exercise program to get the module between two numbers named a and b and print the result
+ * on the console using console.log() function.
+ * 
+ * By Pablo P Fernandes
+ * 
+ */
+
+ let a = 3;
+ let b = 2;
+
+ console.log(a%b);
