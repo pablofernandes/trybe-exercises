8b40e65595197fd128b68b7ba2d621ec726a5fd8
Pablo Fernandes <pabloprates@gmail.com>
2020-11-25 17:59:58 -0300

    The sum program

--- trybe-exercises/exercises/bloco_4/intro-js-log-prog_1/adicao.js
+++ trybe-exercises/exercises/bloco_4/intro-js-log-prog_1/adicao.js
@@ -1,3 +1,11 @@
+/**
+ * This is a exercise program to sum two numbers named a and b and print the result
+ * on the console using console.log() function.
+ * 
+ * By Pablo P Fernandes
+ * 
+ */
+
 let a = 10;
 let b = 15;
 
