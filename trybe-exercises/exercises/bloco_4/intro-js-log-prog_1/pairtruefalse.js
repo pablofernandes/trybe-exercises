2cf12e582eff015037096500d7a202f8ec0fe074
Pablo Fernandes <pabloprates@gmail.com>
2020-11-25 19:39:27 -0300

    The comparison to verify which three given numbers typed by user are at least one pair  program

---
+++ trybe-exercises/exercises/bloco_4/intro-js-log-prog_1/pairtruefalse.js
@@ -0,0 +1,16 @@
+/**
+ * This is a exercise program to verify if one of three numbers given named a, b and c is pair 
+ * and print the result (pair or oven) on the console using console.log() function.
+ * 
+ * By Pablo P Fernandes
+ * 
+ */
+let a = 15;
+let b = 17;
+let c = 113;
+
+if (a%2 && b%2 && c%2){
+    console.log('False');
+} else {
+    console.log('True');
+}
