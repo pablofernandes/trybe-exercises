12f8cc35007d4e2061ebbfe55f99849373ef05f1
Pablo Fernandes <pabloprates@gmail.com>
2020-11-26 17:40:45 -0300

    The array sum  hardly program

---
+++ trybe-exercises/exercises/bloco_4/intro-js-log-prog_2/sum-print-array-hardly.js
@@ -0,0 +1,20 @@
+/**
+ * This is an exercise program to print an array in a dumb way
+ * 
+ * By Pablo P Fernandes
+ */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum;
+sum = numbers[0];
+sum += numbers[1];
+sum += numbers[2];
+sum += numbers[3];
+sum += numbers[4];
+sum += numbers[5];
+sum += numbers[6];
+sum += numbers[7];
+sum += numbers[8];
+sum += numbers[9];
+
+console.log(sum);
