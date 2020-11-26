6ebc56591e454c70e53f252743dbcd19de52eed2
Pablo Fernandes <pabloprates@gmail.com>
2020-11-26 18:11:55 -0300

    The array creator and printer program

---
+++ trybe-exercises/exercises/bloco_4/intro-js-log-prog_2/array-creator-for.js
@@ -0,0 +1,12 @@
+/**
+ * This is an exercise program to create an array of numbers in an array in a for way
+ * 
+ * By Pablo P Fernandes
+ */
+let numbers = [];
+
+for (let index = 0; index < 25; index++) {
+    numbers[index] = index + 1;
+}
+
+console.log(numbers);
