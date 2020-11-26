caffe3770b5e46c321dfd1e112416201eac66894
Pablo Fernandes <pabloprates@gmail.com>
2020-11-26 17:57:21 -0300

    The array greater number program

---
+++ trybe-exercises/exercises/bloco_4/intro-js-log-prog_2/greater-value-array-for.js
@@ -0,0 +1,17 @@
+/**
+ * This is an exercise program to find de greater number
+ * among the numbers in an array in a for way
+ * 
+ * By Pablo P Fernandes
+ */
+let numbers = [5, 9, 3, 19, 70, 8, 70, 2, 35, 27];
+let aux = numbers[0];
+let index;
+
+for (let index = 0; index < numbers.length; index++) {
+  if (numbers[index] >= aux) {
+      aux = numbers[index];
+  }    
+}
+
+console.log('The greater number is ' + aux);
