122fd95e52f40bff427e7bc4ca7af6359247fd6c
Pablo Fernandes <pabloprates@gmail.com>
2020-11-27 13:57:14 -0300

    Correcting somethings program

--- trybe-exercises/exercises/bloco_4/intro-js-log-prog_2/multiplier-array-for.js
+++ trybe-exercises/exercises/bloco_4/intro-js-log-prog_2/multiplier-array-for.js
@@ -1,15 +1,15 @@
 /**
- * This is an exercise program to multiply the numbers  of an array by his neighbor in a for way
+ * This is an exercise program to multiply the numbers  of an array by its neighbor in a for way
  * 
  * By Pablo P Fernandes
  */
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let multiplieNumbers = [];
 for (let index = 0; index < numbers.length; index++) {
-    if (index == numbers.length-1) {
-        multiplieNumbers[index] = numbers[index]*2;
+    if (index == numbers.length - 1) {
+        multiplieNumbers.push(numbers[index]*2);
     } else {
-        multiplieNumbers[index] = numbers[index] * numbers[index+1];
+        multiplieNumbers.push(numbers[index] * numbers[index + 1]);
     }
 }
 
