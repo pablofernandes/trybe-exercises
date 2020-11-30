2920b1ff03c25fb81dafd49f085743211a64a449
Pablo Fernandes <pabloprates@gmail.com>
2020-11-30 18:56:18 -0300

    The index of the smallest number program

---
+++ trybe-exercises/exercises/bloco_4/intro-js-log-prog_4/get-smallestValue.js
@@ -0,0 +1,31 @@
+/**
+ * This is an exercise program to get the index of the smallest number of an array.
+ * 
+ * By Pablo P Fernandes
+ * 
+ */
+
+let testArray = [2,3,6,7,10,1];
+
+console.log('The index of the smallest number is ', smallestNumberIndexFinder(testArray));
+
+
+function smallestNumberIndexFinder(listToFind) {
+   /**
+    * This is an exercise function to sort an array of numbers using Bubble Sort 
+    * ascendenet algorithm in a for way
+    * 
+    * By Pablo P Fernandes
+    */
+   let smaller = listToFind[0];
+   let smallerIndex = 0;
+
+   for (let index = 1; index < listToFind.length; index += 1) {
+       if(listToFind[index] <= smaller) {
+        smallerIndex = index;
+       }
+   }
+
+   return smallerIndex;
+
+}
