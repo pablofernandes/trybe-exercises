9f7632cf1976c9743c8790ff3db3f8c39e14a6ba
Pablo Fernandes <pabloprates@gmail.com>
2021-01-05 15:04:59 -0300

    Starting the first 2021 exercise

---
+++ trybe-exercises/exercises/bloco_6/script.js
@@ -0,0 +1,13 @@
+const states = ['Acre', 'Alagoas', 'Amazonas', 'Amapá', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás',
+'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 
+'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia',
+'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+function stateOptionCreator() {
+    const selectElement = document.getElementById('state');
+
+    for (let index = 0; index < states.length; index += 1) {
+        let optionElement = document.createElement('option');
+        
+    }
+}
