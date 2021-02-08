7239a6b75c681f42ed877e0015d7bb9bca6dd591
Pablo Fernandes <pabloprates@gmail.com>
2021-02-08 17:33:06 -0300

    Exercises 3 and 4

--- trybe-exercises/exercises/bloco_10/day_2/exercisesTwoAndThree.test.js
+++ trybe-exercises/exercises/bloco_10/day_2/exercisesTwoAndThree.test.js
@@ -7,4 +7,15 @@ describe('Unity test to test async working mode with promise', () => {
     it('Should not return a user', () => {
         return expect(findUserById(7)).rejects.toEqual({ error: 'User with 7 not found.' });
     });
+    it('Should return a user. With async/await', async () => {
+        const user = await findUserById(4);
+        expect(user).toEqual({ name: 'Mark' });
+    });
+    it('Should not return a user. With async/await', async () => {
+        try {
+            await findUserById(7);
+        } catch (error) {
+            expect(error).toEqual({ error: 'User with 7 not found.' });
+        }
+    });
 });
